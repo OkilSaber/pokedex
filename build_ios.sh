6fb5d2aa07dd29f9b907ac3438edd126981a682f
slakhdari <okil.lakhdari@kbrwadventure.com>
2023-06-25 20:20:28 +0200

    Add ios build script

---
+++ build_ios.sh
@@ -0,0 +1,10 @@
+flutter build xcarchive
+if [ -d "./build/ios/archive/Runner.xcarchive" ]; then
+    echo "build success"
+    mkdir Payload
+    cp -r ./build/ios/archive/Runner.xcarchive ./Payload/
+    zip -r pokedex.ipa Payload
+    rm -rf Payload
+else
+    echo "build failed"
+fi
