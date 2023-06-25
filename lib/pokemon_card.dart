e2d55e51ecc295f9ca9f87907338ebb29eceed69
OkilSaber <lakhsaber@gmail.com>
2023-06-25 17:53:07 +0200

    Add details ui

--- lib/pokemon_card.dart
+++ lib/pokemon_card.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:pokedex/pokemon_details.dart';
 import 'package:pokedex/structs/pokemon.dart';
 
 extension StringExtension on String {
@@ -16,70 +18,64 @@ class PokemonCard extends StatefulWidget {
 class _PokemonCardState extends State<PokemonCard> {
   @override
   Widget build(BuildContext context) {
-    print(widget.pokemon.name);
-    return Container(
-      decoration: BoxDecoration(
-        color: const Color.fromARGB(92, 255, 255, 255),
-        borderRadius: BorderRadius.circular(15),
-      ),
-      height: 210,
-      child: Column(
-        children: [
-          const SizedBox(height: 10),
-          Text(
-            widget.pokemon.name.capitalize(),
-            style: const TextStyle(
-              color: Colors.white,
-              fontSize: 20,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          CupertinoModalPopupRoute(
+            builder: (context) => PokemonDetails(
+              pokemon: widget.pokemon,
             ),
           ),
-          Image.network(
-            widget.pokemon.sprites.frontDefault,
-            height: 96,
-            fit: BoxFit.fitHeight,
-            // width: 288,
-          ),
-          const SizedBox(height: 10),
-          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-            ...widget.pokemon.types.map(
-              (type) => Container(
-                margin: const EdgeInsets.only(right: 5),
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 10,
-                  vertical: 5,
-                ),
-                decoration: BoxDecoration(
-                  color: const Color.fromARGB(120, 248, 89, 137),
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                child: Text(
-                  type.capitalize(),
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
+        );
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(92, 255, 255, 255),
+          borderRadius: BorderRadius.circular(15),
+        ),
+        height: 210,
+        child: Column(
+          children: [
+            const SizedBox(height: 10),
+            Text(
+              widget.pokemon.name.capitalize(),
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+              ),
+            ),
+            Image.network(
+              widget.pokemon.sprites.frontDefault,
+              height: 96,
+              fit: BoxFit.fitHeight,
+              // width: 288,
+            ),
+            const SizedBox(height: 10),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              ...widget.pokemon.types.map(
+                (type) => Container(
+                  margin: const EdgeInsets.only(right: 5),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 10,
+                    vertical: 5,
+                  ),
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(120, 248, 89, 137),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Text(
+                    type.capitalize(),
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
                   ),
                 ),
               ),
-            ),
-          ]
-              // children: [
-              //   Text(
-              //     "Grass",
-              //     style: const TextStyle(
-              //       color: Colors.white,
-              //       fontSize: 20,
-              //     ),
-              //   ),
-              //   Text(
-              //     "Grass",
-              //     style: const TextStyle(
-              //       color: Colors.white,
-              //       fontSize: 20,
-              //     ),
-              //   )
-              // ],
-              )
-        ],
+            ])
+          ],
+        ),
       ),
     );
   }
