5ac2da1f09f34c7feba35890864866338fc1044f
slakhdari <okil.lakhdari@kbrwadventure.com>
2023-06-25 23:34:14 +0200

    Update infos screen

--- lib/infos.dart
+++ lib/infos.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Infos extends StatefulWidget {
   const Infos({super.key});
@@ -10,10 +12,62 @@ class Infos extends StatefulWidget {
 class _InfosState extends State<Infos> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: CupertinoButton(
-        child: const Text("Test"),
-        onPressed: () {},
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Center(
+          child: CupertinoListSection.insetGrouped(
+            backgroundColor: Colors.black,
+            header: const Text(
+              'Pokedex',
+              style: TextStyle(color: Colors.white),
+            ),
+            children: <CupertinoListTile>[
+              const CupertinoListTile.notched(
+                backgroundColor: Color.fromARGB(255, 28, 28, 30),
+                title: Text(
+                  'Developped by Okil Saber Lakhdari',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              const CupertinoListTile.notched(
+                backgroundColor: Color.fromARGB(255, 28, 28, 30),
+                title: Text(
+                  'Flutter 3.10.5',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              CupertinoListTile.notched(
+                backgroundColor: const Color.fromARGB(255, 28, 28, 30),
+                title: const Text(
+                  'PokeAPI v2',
+                  style: TextStyle(color: Colors.white),
+                ),
+                trailing: const CupertinoListTileChevron(),
+                onTap: () {
+                  launchUrl(
+                    Uri.parse("https://pokeapi.co/"),
+                    mode: LaunchMode.externalApplication,
+                  );
+                },
+              ),
+              CupertinoListTile.notched(
+                backgroundColor: const Color.fromARGB(255, 28, 28, 30),
+                title: const Text(
+                  'Github Repository',
+                  style: TextStyle(color: Colors.white),
+                ),
+                trailing: const CupertinoListTileChevron(),
+                onTap: () {
+                  launchUrl(
+                    Uri.parse("https://github.com/OkilSaber/pokedex"),
+                    mode: LaunchMode.externalApplication,
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
